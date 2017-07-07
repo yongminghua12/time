9a2433b5d4400a3753cc66632bbb130fb0561dee
yong <2219269078@qq.com>
2017-07-07 11:06:07 +0800

    第一次

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php
+	// 模拟一个数据查看请求是否过来
+	// echo "abc";
+	// 获取json文件的数据
+	echo file_get_contents("./student.json");
+	// 获取get请求所传递的参数
+	// echo $_GET["name"];
+	// 获取post请求所传递的参数
+	// echo $_POST["name"];
+?>
